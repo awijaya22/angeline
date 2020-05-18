bec47b4cb46301aed894cf37e4b8b811a26b6649
Angeline <angeline.huang@i2csolution.com>
2020-05-18 22:32:14 +0800

    create header and footer;

---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Footer(){
+    return(
+        <footer>
+            <Container fluid={true}>
+                <Row className={'border-top justify-content-between p-3'}>
+                    <Col className="p-0" md={5} sm={6}>
+                        Angeline Wijaya
+                    </Col>
+                    <Col className={'p-0 d-flex justify-content-end'} md={5}>
+                        This site is made by Angeline
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import '../css/Header.css'
+
+class Header extends React.Component{
+    render(){
+            return(
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand>Angeline</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link>Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            )
+    }
+}
+export default Header
