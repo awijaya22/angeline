9cfc40ceb6f197cccc923a0058b3354af5418178
Angeline <angeline.huang@i2csolution.com>
2021-02-06 17:27:26 +0800

    make year dynamic and update about me

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,12 +5,13 @@ import Col from 'react-bootstrap/Col';
 import '../css/Footer.css'
 
 function Footer(){
+    var year = new Date().getFullYear();
     return(
         <footer>
             <Container fluid={true}>
                 <Row className={'border-top justify-content-center p-3'}>
                     <Col className={'p-0 d-flex justify-content-center'} md={5}>
-                        © 2020 Angeline's Personal Website by Angeline
+                        © {year} Angeline's Personal Website by Angeline
                     </Col>
                 </Row>
             </Container>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,9 +10,13 @@ function About(){
                 <Row className={'justify-content-center'}>
                     <Col md={6}>
                         <p>
-                            Hard working computer science student with 2+ years experience intern in big-scale projects.
-                            Enthusiast in developing software that is maintainable and testable.
-                            Always make software that helps the world become better.
+                            Ever since I started to focus on coding on 2016, I found myself keep on wanting to learn more. I finally found something that keep the spark in me. I love the fact that i could create things and solving problem that I encountered in everyday life.
+                        </p>
+                        <p>
+                            I first encountered software engineering when i took the class in my junior year. There is a bunch of theory that is taught in the class. But there is always this question hanging in my mind, When and where to implement this theory?
+                        </p>
+                        <p>
+                            Until I start my internship in this startup that help me understand the importance of the software engineering knowledge. Since then my passion and curiosity in software engineering keeps on growing. Technology is limitless and I love to keep on learning and explore it. There are so many things that we could improved and built. And for this, I am very grateful to be a part of this world.
                         </p>
                     </Col>
                 </Row>
